e96c211abf92c497dd4ac6aacb38116b4d06cf96
Sarthak Kaushik <skkaushik212@gmail.com>
2024-06-06 20:44:57 +0530

    Cors added

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import slotRouter from "./router/slot";
 import "dotenv/config";
 import { checkUserCode } from "./middleware";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app: Express = express();
 const PORT: number = parseInt(process.env.PORT as string, 10) || 4000;
 (async () => {
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+  app.use(cors());
   await mongoose.connect(process.env.DATABASE_URL!);
   app.use("/user", userRouter);
   app.use(checkUserCode);
